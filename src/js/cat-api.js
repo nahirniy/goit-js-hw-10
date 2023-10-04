13653f98640fe093a85cf4276d0d10668a2648aa
nahirniy <nahirniyy@gmail.com>
2023-10-04 18:11:45 +0300

    update code for cat-api

--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -6,21 +6,11 @@ axios.defaults.headers.common['x-api-key'] =
 const BASE_URL = `https://api.thecatapi.com/v1`;
 
 export function fetchBreeds() {
-  return axios.get(`${BASE_URL}/breeds`).then(resp => {
-    if (resp.status !== 200) {
-      return;
-    }
-    return resp.data;
-  });
+  return axios.get(`${BASE_URL}/breeds`).then(resp => resp.data);
 }
 
 export function fetchCatByBreed(breedId) {
   return axios
     .get(`${BASE_URL}/images/search?breed_ids=${breedId}`)
-    .then(resp => {
-      if (resp.status !== 200) {
-        return;
-      }
-      return resp.data[0];
-    });
+    .then(resp => resp.data[0]);
 }
